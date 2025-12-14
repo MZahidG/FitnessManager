31f7704791a8c7661a1816506c7bf9f77ffa8c27
MZahidG <118001804+MZahidG@users.noreply.github.com>
2025-12-14 23:55:13 +0300

    Add Data information

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using webOdev.Models;
+
+namespace webOdev.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Antrenor> Antrenorler { get; set; }
+        public DbSet<DersSeans> DersSeanslari { get; set; }
+        public DbSet<Randevu> Randevular { get; set; }
+    }
+}
