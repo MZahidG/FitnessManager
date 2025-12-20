a48b288ec3830e4d223232bef6dfb217041f8ac9
MZahidG <118001804+MZahidG@users.noreply.github.com>
2025-12-20 22:59:07 +0300

    add models

---
+++ Models/Randevu.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webOdev.Models
+{
+    public class Randevu
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime OlusturulmaTarihi { get; set; } = DateTime.UtcNow;
+        public bool OnayDurumu { get; set; } = false;
+
+        public int DersSeansId { get; set; }
+        [ForeignKey("DersSeansId")]
+        public DersSeans? DersSeans { get; set; }
+
+        public string UyeId { get; set; }
+        [ForeignKey("UyeId")]
+        public IdentityUser? Uye { get; set; }
+    }
+}
